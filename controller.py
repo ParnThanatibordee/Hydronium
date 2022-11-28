691c1eb5602f7a7c985748be31f9f4ccbb190ede
ParnThanatibordee <parn8946@gmail.com>
2022-11-29 03:56:50 +0700

    edit get_water_consume & get_water_consume_details in controller.py

--- controller.py
+++ controller.py
@@ -7,6 +7,8 @@ from config import OPENAPI_STUB_DIR, DB_HOST, DB_USER, DB_PASSWD, DB_NAME
 sys.path.append(OPENAPI_STUB_DIR)
 from swagger_server import models
 
+W = 18
+
 pool = PooledDB(creator=pymysql,
                 host=DB_HOST,
                 user=DB_USER,
@@ -39,10 +41,35 @@ def get_sensor_details(source):
         abort(404)
 
 def get_water_consume():
-    return 'get_water_consume'
+    with pool.connection() as conn, conn.cursor() as cs:
+        cs.execute("""
+            SELECT ts, source, value
+            FROM SoftWet_sensor
+            WHERE source=co2signal
+        """)
+        result = [] 
+        for row in cs.fetchall():
+            ts, source, value = row
+            co2 = (float(value)/100)/W
+            result.append(models.WaterConsume(ts, co2)) 
+
+        return result
 
 def get_water_consume_details(ts):
-    return 'get_water_consume_details'
+    with pool.connection() as conn, conn.cursor() as cs:
+        cs.execute("""
+            SELECT ts, source, value
+            FROM SoftWet_sensor
+            WHERE source=co2signal AND ts=%s
+        """, [ts])
+        result = cs.fetchone()
+    if result:
+        ts, source, value = result
+        co2 = (float(value)/100)/W
+        
+        return models.WaterConsume(ts, co2)
+    else:
+        abort(404)
 
 def get_watering_next_time():
     return 'get_watering_next_time'
