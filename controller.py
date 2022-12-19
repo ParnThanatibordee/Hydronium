327edc842364f58fb47ccde15f66e4c26000963b
ParnThanatibordee <parn8946@gmail.com>
2022-12-19 16:05:40 +0700

    edit get_watering_next_time

--- controller.py
+++ controller.py
@@ -6,15 +6,17 @@ from config import OPENAPI_STUB_DIR, DB_HOST, DB_USER, DB_PASSWD, DB_NAME
 
 sys.path.append(OPENAPI_STUB_DIR)
 from swagger_server import models
+from datetime import timedelta
 
 W = 18
+PINNED_DEPTH = 6  # cm
 
 pool = PooledDB(creator=pymysql,
                 host=DB_HOST,
                 user=DB_USER,
                 password=DB_PASSWD,
                 database=DB_NAME,
-                maxconnections=1,
+                maxconnections=100,
                 blocking=True)
 
 def get_sensors():
@@ -75,9 +77,57 @@ def get_watering_next_time():
             WHERE source='tmd'
         """)
         fetch = cs.fetchall()
-        ts, source, value = fetch[len(fetch)-1]
-        if value >= 10:
-            return "The weather tends to rain, so you don't have to water yet."
+
+        if len(fetch) > 0:
+            ts, source, value = fetch[len(fetch)-1]
         else:
-            return models.WaterAgain(ts) 
+            value = 0
+
+        if value >= 10:
+            return models.WaterAgain("The weather tends to rain, so you don't have to water yet.")
+        else:
+            cs.execute("""
+                SELECT ts, source, value
+                FROM SoftWet_sensor
+                WHERE source='soil'
+            """)
+            fetch = cs.fetchall()
+
+            if len(fetch) > 0:
+                last_ts, source, value = fetch[len(fetch)-1]
+                new_humidity = (float(value)/100) * PINNED_DEPTH
+            else:
+                last_ts = "No data."
+                new_humidity = 0
+
+            # next hour to watering
+            cs.execute("""
+            SELECT ts, source, value
+            FROM SoftWet_sensor
+            WHERE source='co2signal'
+            """)
+            result = [] 
+            for row in cs.fetchall():
+                ts, source, value = row
+                float_value = float(value)
+                if float_value > 0:
+                    result.append(float_value)
+
+            if len(result) > 0:
+                water_consume = result[-1]
+                co2 = (water_consume/100) * W
+
+                next_hr = (new_humidity/co2) * W
+            else:
+                next_hr = 0
+
+            # print('*', next_hr)
+
+            # next_ts = ts + next_hr
+            next_ts = last_ts + timedelta(hours=next_hr)
+
+            # format datetime.datetime more readable
+            new_ts = next_ts.strftime("%d/%m/%Y %H:%M:%S")
+
+            return models.WaterAgain(new_ts) 
 
